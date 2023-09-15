078520530ef61bbb38fdc7a1260beaaee75cb4e0
Sebastian Giannini <sebastiangiannini95@gmail.com>
2023-09-14 21:08:50 -0300

    creacion del repositorio - primer push

---
+++ Proyecto-JAVAWEBAPP/proyecto-web-DOMINation/src/main/java/com/mycompany/proyecto/web/domination/RegistroServlet.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyecto.web.domination;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ * @author giann
+ */
+public class RegistroServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+    }
+    
+}
